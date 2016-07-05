ed0a0cdf73066805353eb959c80b045b9c8a1916
Omar Saad <omar_saad12@hotmail.com>
2016-07-05 02:38:07 +0200

    added jquery for use later

--- object.php
+++ object.php
@@ -3,3 +3,17 @@
 echo "";
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/master.css" charset="utf-8">
+        <script src="scripts/jquery-migrate-1.4.1.min.js"></script>
+        <script src="scripts/jquery-3.0.0.min.js"></script>
+    </head>
+    <body>
+        <div id="main">
+            
+        </div>
+    </body>
+</html>
